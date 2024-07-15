28c593c68d6403956ed28bea313c40011128b2fc
ayazb7 <60544937+ayazb7@users.noreply.github.com>
2024-07-15 16:28:33 +0100

    Adding firebase analytics

--- src/App.js
+++ src/App.js
@@ -6,6 +6,22 @@ import Projects from './projects/Projects';
 import Footer from './components/Footer';
 import './App.css';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDWL6tjNFtbtl_oHLkBwSOL6enbVgHE07A",
+  authDomain: "portfolio-c2607.firebaseapp.com",
+  projectId: "portfolio-c2607",
+  storageBucket: "portfolio-c2607.appspot.com",
+  messagingSenderId: "174740778775",
+  appId: "1:174740778775:web:511a71d66a75944de5d152",
+  measurementId: "G-1Y1739XJEQ"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
   return (
     <div className="App">
